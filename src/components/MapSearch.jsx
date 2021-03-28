fe51ab4ab8cc3aa9f4f72373499cb9a8f8733deb
kws_kipis <kws@kipis.co.kr>
2021-03-28 20:26:23 +0900

    진행

--- src/components/MapSearch.jsx
+++ src/components/MapSearch.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import MapSearchListItem from "./MapSearchListItem";
 const { kakao } = window;
 
 const MapSearch = () => {
@@ -10,8 +11,10 @@ const MapSearch = () => {
   };
 
   const searchResult = (data, status, pagination) => {
-    if (status === kakao.maps.services.Status.OK) {
+    console.log(data);
+    console.log(pagination);
 
+    if (status === kakao.maps.services.Status.OK) {
       setResult(data);
       // // 정상적으로 검색이 완료됐으면
       // // 검색 목록과 마커를 표출합니다
@@ -19,29 +22,24 @@ const MapSearch = () => {
 
       // // 페이지 번호를 표출합니다
       // displayPagination(pagination);
-
-  } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
-
-      alert('검색 결과가 존재하지 않습니다.');
+    } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+      alert("검색 결과가 존재하지 않습니다.");
       return;
-
-  } else if (status === kakao.maps.services.Status.ERROR) {
-
-      alert('검색 결과 중 오류가 발생했습니다.');
+    } else if (status === kakao.maps.services.Status.ERROR) {
+      alert("검색 결과 중 오류가 발생했습니다.");
       return;
-
-  }
+    }
   };
 
   const searchLocation = (e) => {
     e.preventDefault();
-
-    // 키워드 검색을 요청하는 함수입니다
-    const ps = new kakao.maps.services.Places();
     if (!keyword.replace(/^\s+|\s+$/g, "")) {
       alert("키워드를 입력해주세요!");
       return false;
     }
+
+    // 키워드 검색을 요청하는 함수입니다
+    const ps = new kakao.maps.services.Places();
     ps.keywordSearch(keyword, searchResult);
   };
 
@@ -53,9 +51,11 @@ const MapSearch = () => {
           <button type="submit">검색</button>
         </div>
       </form>
-      {result.map((list) => (
-        <div key={list.address_name}>{list.address_name}</div>
-      ))}
+      <div className="maplist">
+        {result.map((list) => (
+          <MapSearchListItem key={list.id} item={list} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/MapSearchListItem.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+const MapSearchList = (props) => {
+
+  useEffect(()=>{
+  })
+  
+  return <div className="map-list-item">{props.item.address_name}</div>;
+};
+
+export default MapSearchList;
